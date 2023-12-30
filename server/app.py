7ec08205e06aeee53715c19917d72d94a5e7cf5c
KELVIN MURITHI <kelvinmurithi@live.com>
2023-12-30 19:17:43 +0300

    Implement user authentication and recipe CRUD endpoints
    Add POST /signup endpoint to create a new user.
    
    Add GET /check_session endpoint to check if user is logged in.
    
    Add POST /login endpoint for user login.
    
    Add DELETE /logout endpoint for user logout
    
    Add GET /recipes endpoint to get recipes for logged in user.
    
    Add POST /recipes endpoint to create a new recipe for logged in user.
    
    Use Flask session to track logged in user.
    
    Add password hashing and authentication for users.
    
    Return appropriate error responses for unauthorized access.

--- server/app.py
+++ server/app.py
@@ -8,19 +8,88 @@ from config import app, db, api
 from models import User, Recipe
 
 class Signup(Resource):
-    pass
+    def post(self):
+        json_data = request.get_json()
+
+        username = json_data.get('username')
+        password = json_data.get('password')
+        image_url = json_data.get('image_url')
+        bio = json_data.get('bio')
+
+        if username and password:
+            new_user = User(
+                username = username, 
+                image_url = image_url,
+                bio = bio
+            )
+            new_user.password_hash = password
+            db.session.add(new_user)
+            db.session.commit()
+
+            session['user_id'] = new_user.id
+            return new_user.to_dict(), 201
+        return {}, 422
+    
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        if session.get('user_id'):
+            user = User.query.filter(User.id == session['user_id']).first()
+            return user.to_dict(), 200
+        else:
+            return {}, 401
+
 
 class Login(Resource):
-    pass
+    def post(self):
+        request_json = request.get_json()
+        username = request_json.get('username')
+        password = request_json.get('password')
+        user = User.query.filter(User.username == username).first()
+
+        if user:
+            if user.authenticate(password):
+                session['user_id'] = user.id
+                return user.to_dict(), 200
+        
+        return {'error': 'Unauthorized'}, 401
+    
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        if session.get('user_id'):
+
+            session['user_id'] = None
+            return {}, 204
+        return {'error': '401'} , 401
 
 class RecipeIndex(Resource):
-    pass
+    def get(self):
+        if session.get('user_id'):
+            user = User.query.filter(User.id == session['user_id']).first()
+            return [recipe.to_dict() for recipe in user.recipes], 200
+        return {'error': '401: Unauthorized, please log in'}, 401
+
+    def post(self):
+        if session.get('user_id'):
+            json_data = request.get_json()
+            title = json_data.get('title')
+            instructions = json_data.get('instructions')
+            minutes_to_complete = json_data.get('minutes_to_complete')
+
+            try:
+                new_recipe = Recipe(
+                    title=title,
+                    instructions=instructions,
+                    minutes_to_complete=minutes_to_complete,
+                    user_id=session['user_id'],
+                )
+                db.session.add(new_recipe)
+                db.session.commit()
+                return new_recipe.to_dict(), 201
+            except IntegrityError:
+                return {'error': '422: Recipe not valid'}, 422
+        return {'error': '401: User not logged in'}, 401
 
 api.add_resource(Signup, '/signup', endpoint='signup')
 api.add_resource(CheckSession, '/check_session', endpoint='check_session')
